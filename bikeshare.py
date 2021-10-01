4b3defc06fbe8d42a9621e39f1c69d9184072f30
Fred Kumi <ratti10gh@yahoo.com>
2021-10-01 01:30:37 +0000

    Refactor: rename method name to be more descriptive

--- bikeshare.py
+++ bikeshare.py
@@ -65,7 +65,7 @@ def load_data(city, month, day):
     return df
 
 
-def time_stats(df):
+def time_statistics(df):
     """Displays statistics on the most frequent times of travel."""
 
     print('\nCalculating The Most Frequent Times of Travel...\n')
@@ -188,7 +188,7 @@ def main():
         city, month, day = get_filters()
         df = load_data(city, month, day)
 
-        time_stats(df)
+        time_statistics(df)
         station_stats(df)
         trip_duration_stats(df)
         user_stats(df, city)
